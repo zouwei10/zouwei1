9139de73794223e251fe456dd1693a8128ad18db
chenxw <cxwokok123@126.com>
2016-11-08 23:22:49 +0800

    ooooooooooooooo

--- src/weixin11/Test.java
+++ src/weixin11/Test.java
@@ -3,7 +3,7 @@ package weixin11;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("-----88888----777777--zouwei------------");
+		System.out.println("-----88888----777777--zouwei333------------");
 
 	}
 
