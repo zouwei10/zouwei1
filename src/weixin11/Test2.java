770fe3e85c41a1c7a8a2aaec602394c195b11122
zouw2 <343854933@qq.com>
2016-11-16 00:57:12 -0800

    test2

--- src/weixin11/Test2.java
+++ src/weixin11/Test2.java
@@ -3,7 +3,7 @@ package weixin11;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// add samething TODO Auto-generated method stub
 System.out.println("00000000000000000");
 	}
 
