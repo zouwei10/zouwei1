ff450a986b5bc5efc240c0499b18766fcbff0bc5
zouwei <2270792772@qq.com>
2016-11-16 17:16:49 +0800

    xxxxxxxxxxxxxxxxxxfffdfdfdfddffd

--- src/weixin11/Test2.java
+++ src/weixin11/Test2.java
@@ -3,7 +3,7 @@ package weixin11;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// add samething TODO Auto-generated method stub
+		// add something TODO Auto-generated method stub
 System.out.println("00000000000000000");
 	}
 
