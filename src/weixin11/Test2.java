00a0b6d440921858fa8ba6c44abd06eb214804ae
zouwei <2270792772@qq.com>
2016-11-24 17:21:40 +0800

    0000000000000

--- src/weixin11/Test2.java
+++ src/weixin11/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// add something TODO Auto-generated method stub
-System.out.println("00000000000000000");
+System.out.println("00000000000999000000");
 	}
 
 }
