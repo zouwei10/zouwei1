e1210c210d1e68e073f3dad7009b19db4bcb53c5
zouwei <2270792772@qq.com>
2016-11-24 16:09:05 +0800

    ggggggggggggggg

--- src/weixin11/Test.java
+++ src/weixin11/Test.java
@@ -3,7 +3,7 @@ package weixin11;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("-hadoop2----88888----777777--zouwei333--dddooo----------");
+		System.out.println("-hadoop2----88888----777777--zouwei3dddddd33--dddooo----------");
 
 	}
 
